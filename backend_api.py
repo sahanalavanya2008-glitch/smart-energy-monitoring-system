d26dbf255833de97d709a2bc9faf92be105f9c6c
sahanalavanya2008-glitch <sahanalavanya2008@gmail.com>
2026-07-07 14:25:07 +0530

    Update backend_api.py

--- backend_api.py
+++ backend_api.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 from datetime import datetime
+import sqlite3
 
 app = FastAPI()
 
-energy_data = []
+DATABASE = "energy_data.db"
+
+
+def connect_db():
+    return sqlite3.connect(DATABASE)
+
 
 @app.get("/")
 def home():
@@ -15,19 +21,41 @@ def home():
 @app.post("/energy")
 def add_energy(device_id: str, usage: float, battery: int):
 
-    data = {
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    connection = connect_db()
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        INSERT INTO energy_readings
+        (device_id, energy_usage, battery_percentage, timestamp)
+        VALUES (?, ?, ?, ?)
+        """,
+        (device_id, usage, battery, timestamp)
+    )
+
+    connection.commit()
+    connection.close()
+
+    return {
         "device_id": device_id,
-        "energy_usage_kwh": usage,
-        "battery_percentage": battery,
-        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        "energy_usage": usage,
+        "battery": battery,
+        "timestamp": timestamp
     }
-
-    energy_data.append(data)
-
-    return data
 
 
 @app.get("/energy")
 def get_energy():
 
-    return energy_data
+    connection = connect_db()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM energy_readings")
+
+    data = cursor.fetchall()
+
+    connection.close()
+
+    return data
